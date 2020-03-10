a91231fbab3ca763721c64d36431419db9851d79
Alex Noyanov <alex.noyanov@gmail.com>
2020-03-10 23:15:37 +0300

    Simple application with two pages

---
+++ SimpleApp/app/src/main/java/com/example/simpleapp/Page1.java
@@ -0,0 +1,9 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page1 extends AppCompatActivity {
+
+}
---
+++ SimpleApp/app/src/main/java/com/example/simpleapp/Page2.java
@@ -0,0 +1,7 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page2 extends AppCompatActivity {
+
+}
