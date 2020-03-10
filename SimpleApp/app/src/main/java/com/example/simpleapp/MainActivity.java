50124fadc0117f0918bb9050cc0adcd3bd11a58b
Alex Noyanov <alex.noyanov@gmail.com>
2020-03-10 23:16:11 +0300

    Simple Android application

---
+++ SimpleApp/app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,Page1.class);   // Переход на страницу Page1 с главной
+
+
+
+    }
+}
